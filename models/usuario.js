1c92dca321f3ed7f38d70093da79851e218dccb7
Kaio Duarte <duartedossantos@gmail.com>
2018-05-05 03:05:30 -0300

    fix model usuario

--- models/usuario.js
+++ models/usuario.js
@@ -1,41 +1,40 @@
-const { sequelize, Sequelize } = require("./index");
-
-sequelize.define("usuario", {
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  nome: {
-    type: Sequelize.STRING(100),
-    allowNull: false
-  },
-  email: {
-    type: Sequelize.STRING(50),
-    allowNull: false
-  },
-  senha: {
-    type: Sequelize.STRING(90),
-    allowNull: false
-  },
-  endereco: {
-    type: Sequelize.STRING(100),
-    allowNull: false
-  },
-  cnpj: {
-    type: Sequelize.STRING(14),
-    allowNull: false
-  },
-  telefone: {
-    type: Sequelize.STRING(11),
-    allowNull: false
-  },
-  cidade: {
-    type: Sequelize.STRING(40),
-    allowNull: false
-  },
-  estado: {
-    type: Sequelize.STRING(2),
-    allowNull: false
-  }
-});
+module.exports = (sequelize, types) =>
+  sequelize.define("usuario", {
+    id: {
+      type: types.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    nome: {
+      type: types.STRING(100),
+      allowNull: false
+    },
+    email: {
+      type: types.STRING(50),
+      allowNull: false
+    },
+    senha: {
+      type: types.STRING(90),
+      allowNull: false
+    },
+    endereco: {
+      type: types.STRING(100),
+      allowNull: false
+    },
+    cnpj: {
+      type: types.STRING(14),
+      allowNull: false
+    },
+    telefone: {
+      type: types.STRING(11),
+      allowNull: false
+    },
+    cidade: {
+      type: types.STRING(40),
+      allowNull: false
+    },
+    estado: {
+      type: types.STRING(2),
+      allowNull: false
+    }
+  });
