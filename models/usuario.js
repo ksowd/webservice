b7e4a030987ccc29e77829f5d835777099e33fc6
Kaio Duarte <duartedossantos@gmail.com>
2018-05-05 05:11:29 -0300

    update usuario model and users router

--- models/usuario.js
+++ models/usuario.js
@@ -1,40 +1,93 @@
-module.exports = (sequelize, types) =>
-  sequelize.define("usuario", {
-    id: {
-      type: types.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
+const bcrypt = require("bcryptjs");
+
+module.exports = (sequelize, types) => {
+  return sequelize.define(
+    "usuario",
+    {
+      id: {
+        type: types.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      nome: {
+        type: types.STRING(100),
+        allowNull: false,
+        validate: {
+          notEmpty: true
+        }
+      },
+      email: {
+        type: types.STRING(50),
+        unique: true,
+        allowNull: false,
+        validate: {
+          isEmail: true
+        }
+      },
+      senha: {
+        type: types.STRING(90),
+        allowNull: false,
+        validate: {
+          isLength(value) {
+            if (value.length < 6)
+              throw new Error("Senha precisa ter no mínimo 6 caracteres");
+          }
+        }
+      },
+      endereco: {
+        type: types.STRING(100),
+        allowNull: false,
+        validate: {
+          notEmpty: true
+        }
+      },
+      cnpj: {
+        type: types.STRING(14),
+        unique: true,
+        allowNull: false,
+        validate: {
+          notEmpty: true
+        }
+      },
+      telefone: {
+        type: types.STRING(11),
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+          hasOnlyNumbers(value) {
+            Array.from(value).map(i => {
+              if (isNaN(parseInt(i, 10))) throw new Error("Caractere inválido");
+            });
+          }
+        }
+      },
+      cidade: {
+        type: types.STRING(40),
+        allowNull: false,
+        validate: {
+          notEmpty: true
+        }
+      },
+      estado: {
+        type: types.STRING(2),
+        allowNull: false,
+        validate: {
+          notEmpty: true
+        }
+      }
     },
-    nome: {
-      type: types.STRING(100),
-      allowNull: false
-    },
-    email: {
-      type: types.STRING(50),
-      allowNull: false
-    },
-    senha: {
-      type: types.STRING(90),
-      allowNull: false
-    },
-    endereco: {
-      type: types.STRING(100),
-      allowNull: false
-    },
-    cnpj: {
-      type: types.STRING(14),
-      allowNull: false
-    },
-    telefone: {
-      type: types.STRING(11),
-      allowNull: false
-    },
-    cidade: {
-      type: types.STRING(40),
-      allowNull: false
-    },
-    estado: {
-      type: types.STRING(2),
-      allowNull: false
+    {
+      hooks: {
+        beforeCreate: usuario => {
+          bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(usuario.senha, salt, (err, hash) => {
+              if (err) throw err;
+              usuario.senha = hash;
+              usuario.save();
+            });
+          });
+        }
+      }
     }
-  });
+  );
+};
