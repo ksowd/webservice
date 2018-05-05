ede37b1f0c8637f416d694b34300b19307574f29
Diego Henrique de Abreu <abreu.diego94@gmail.com>
2018-05-05 04:45:34 -0300

    Corrigindo o arquivo de config

--- models/index.js
+++ models/index.js
@@ -11,18 +11,12 @@ var db = {};
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  console.log(config);
   var sequelize = new Sequelize(
     config.database,
     config.username,
     config.password,
     {
-      username: config.username,
-      password: config.password,
-      database: config.database,
-      host: 'mysql642.umbler.com',
-      dialect: 'mysql',
-      port: '41890',
+      ...config,
       define: {
         timestamps: false,
         freezeTableName: true
