4639cc2a250203e9c31b418ead06977ce97d9355
Diego Henrique de Abreu <abreu.diego94@gmail.com>
2018-05-05 02:22:36 -0300

    Criando as configurações do banco

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ var app = express();
 
 // view engine setup
 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
